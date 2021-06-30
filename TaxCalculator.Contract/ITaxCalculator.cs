1f05910e04d0fc3dce57e8b8a2d8583afbd66f2a
John Shoffner <jshoffner29@hotmail.com>
2021-06-30 12:01:32 -0400

    Beginning code structure.

---
+++ TaxCalculator.Contract/ITaxCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace TaxCalculator.Contract
+{
+    /// <summary>
+    /// Only supports Tax by Location
+    /// Calculate the taxes for the order
+    /// </summary>
+    public interface ITaxCalculator
+    {
+        // Get the tax rate for a location
+        decimal GetTaxRateForLocation(string zipcode);
+
+        // Get taxes for the order
+        void GetTaxForOrder();
+    }
+}
---
+++ TaxCalculator.SupportService/TaxCalculatorTaxJar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxCalculator.Contract;
+using Taxjar;
+
+namespace TaxCalculator.SupportService
+{
+    public class TaxCalculatorTaxJar : ITaxCalculator
+    {
+        private readonly TaxjarApi client;
+
+        public TaxCalculatorTaxJar()
+        {
+            var apikey = "5da2f821eee4035db4771edab942a4cc";
+            client = new TaxjarApi(apikey);
+        }
+
+        public void GetTaxForOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetTaxRateForLocation(string zipcode)
+        {
+            //var rates = client.RatesForLocation("90404-3370");
+            var rates = client.RatesForLocation(zipcode);
+
+            return rates.StateRate;
+        }
+    }
+}
